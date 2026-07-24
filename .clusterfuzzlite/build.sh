2949d320ef43a770124faf7a8f0b321c4bc9539d
Richard Slater <github@richard-slater.co.uk>
2026-07-24 22:29:35 +0100

    fix: correct fuzz target paths in ClusterFuzzLite build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -2,7 +2,7 @@
 # Copyright 2026 Skills contributors
 # ClusterFuzzLite build script: copies fuzz targets into $OUT.
 
-for fuzzer in $SRC/skills/fuzzing/fuzz_*.py; do
+for fuzzer in $SRC/fuzzing/fuzz_*.py; do
   fuzzer_name=$(basename "$fuzzer")
   cp "$fuzzer" "$OUT/$fuzzer_name"
 done
